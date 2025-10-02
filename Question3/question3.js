4e84d7b9bba073c4a8f12d69d4c26c6bfb7d0e25
DistinctDynamo <nguyensteven578@gmail.com>
2025-10-02 16:46:53 -0400

    question 3 50% complete

--- Question3/question3.js
+++ Question3/question3.js
@@ -1,11 +1,14 @@
 const fs = require('fs')
 const path = require('path')
 
-const logPath = './logs'
+const Paths ={
+    log:'./logs',
+    file:`./logs/file.txt`,
+} 
 
 function checkDir(path){
-    if(fs.existsSync(path)==false){
-        fs.mkdir(logPath,{recursive: true},(error)=>{
+    if(fs.existsSync(Paths.log)==false){
+        fs.mkdir(Paths.log,{recursive: true},(error)=>{
             if (error){
                 console.log(error);
                 return false;
@@ -19,4 +22,23 @@ function checkDir(path){
     }
 }
 
+function createFiles(path){
+    filenum = 0
+    if(fs.existsSync(Paths.log)==true){
+        for(i=0;i<10;i++){
+            fs.appendFile(`./logs/file${filenum}.txt`,`logs${filenum}.txt`,(error)=>{
+                if(error){
+                    console.log(error)
+                } else{
+                    console.log("Files already created")
+                }
+            })
+            filenum++
+        }
+    }
+}
 
+
+
+checkDir(Paths.log)
+createFiles(Paths.log)
