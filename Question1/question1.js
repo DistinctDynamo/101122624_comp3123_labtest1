d4ba7f3615817b6e6e5c0eef1acfd88cf170009a
DistinctDynamo <nguyensteven578@gmail.com>
2025-10-02 15:20:19 -0400

    completed question 1

--- Question1/question1.js
+++ Question1/question1.js
@@ -1,1 +1,20 @@
 const mixedArray=['PIZZA',10,true,25,false,'Wings']
+
+function lowerCaseWords(array){
+    temp = []
+    temp2 = []
+    for(i=0;i<array.length;i++){
+        if(typeof(array[i])=='string'){
+            temp.push(array[i])
+        }
+    }
+
+    for(i=0;i<temp.length;i++){
+        word=temp[i]
+        temp2.push(word.toLowerCase())
+    }
+
+    return temp2
+}
+
+console.log(lowerCaseWords(mixedArray))
